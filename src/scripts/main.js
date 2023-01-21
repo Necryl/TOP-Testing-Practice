8818f99d9440527bd816cb1081364c5fc94f94b5
Necryl <necrylthewayfarer@gmail.com>
2023-01-21 18:55:13 +0530

    write tests for the calculator object, build it and than pass the test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,4 +13,11 @@ function reverseString(text) {
   }, "");
 }
 
-module.exports = { capitalize, reverseString };
+const calculator = {
+  add: (num1, num2) => num1 + num2,
+  subtract: (num1, num2) => num1 - num2,
+  divide: (num1, num2) => num1 / num2,
+  multiply: (num1, num2) => num1 * num2,
+};
+
+module.exports = { capitalize, reverseString, calculator };
--- src/scripts/tests/main.test.js
+++ src/scripts/tests/main.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./../main";
+import { capitalize, reverseString, calculator } from "./../main";
 
 test("capitalize, capitalize the first char of a given string", () => {
   expect(capitalize("abcd")).toBe("Abcd");
@@ -8,4 +8,15 @@ test("capitalize, capitalize the first char of a given string", () => {
 test("reverseString() takes a string and returns it reversed", () => {
   expect(reverseString("abcd")).toBe("dcba");
   expect(reverseString("hohoho")).toBe("ohohoh");
+});
+
+test("'calculator' is an object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.", () => {
+  expect(calculator.add(2, 2)).toBe(4);
+  expect(calculator.add(3, 5)).toBe(8);
+  expect(calculator.subtract(2, 2)).toBe(0);
+  expect(calculator.subtract(3, 8)).toBe(-5);
+  expect(calculator.divide(6, 3)).toBe(2);
+  expect(calculator.divide(16, 4)).toBe(4);
+  expect(calculator.multiply(4, 8)).toBe(32);
+  expect(calculator.multiply(9, 3)).toBe(27);
 });
