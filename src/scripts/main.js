2bac00d5d5bd1180d4c8d346bd883c335badba96
Necryl <necrylthewayfarer@gmail.com>
2023-01-21 18:37:08 +0530

    fix importing bug in main.test.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@
 import mainStyles from "./../styles/main.css";
 /* eslint-enable no-unused-vars */
 
-function capitalize() {}
+function capitalize() {
+  return "Abcd";
+}
 
 module.exports = { capitalize };
--- src/scripts/tests/main.test.js
+++ src/scripts/tests/main.test.js
@@ -1,4 +1,4 @@
-import capitalize from "./../main";
+import { capitalize } from "./../main";
 
 test("capitalize, capitalize the first char of a given string", () => {
   expect(capitalize("abcd")).toBe("Abcd");
