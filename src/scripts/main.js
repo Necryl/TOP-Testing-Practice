4c69fb2a5f5743ab62b4b82e25d4266d7df5d77d
Necryl <necrylthewayfarer@gmail.com>
2023-01-21 18:39:20 +0530

    pass the test for the capitalize function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,8 @@
 import mainStyles from "./../styles/main.css";
 /* eslint-enable no-unused-vars */
 
-function capitalize() {
-  return "Abcd";
+function capitalize(text) {
+  return text[0].toUpperCase() + text.slice(1);
 }
 
 module.exports = { capitalize };
